0cd57e6c6362afbd20b725b104de8990d6c33df1
JorgeDecker1 <jdecker8@email.cpcc.edu>
2024-09-18 11:16:55 -0400

    descriptive
    Add a print statement explaining that the program was modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.print("Hello World!");
+      System.out.print("Program modified, but able to revert to original version using Github Desktop");
    }
 }
